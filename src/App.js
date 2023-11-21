0e89eb9f667f16c62243b864979cc709ab6fe2d4
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-11-21 19:58:44 -0300

    Clase 2 Curso de React.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './platzi.webp';
+import logo from './Kono_Aqua.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
+          Puto el que lee
         </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
+  );
+}
+
+function TodoItem() {
+  return(
+    <li>
+      <span>V</span>
+      <p>Ir a Mar del Plata</p>
+      <span>X</span>
+    </li>
   );
 }
 
