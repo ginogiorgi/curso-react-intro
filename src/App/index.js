dc2375c5b049385a53f978e09770013e3dc093e7
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-01 14:28:29 -0300

    Clase 16 Curso de React.js

--- src/App/index.js
+++ src/App/index.js
@@ -1,30 +1,10 @@
-import { TodoCounter } from './TodoCounter'
-import { TodoSearch } from './TodoSearch'
-import { TodoList } from './TodoList'
-import { TodoItem } from './TodoItem'
-import { CreateTodoButton } from './CreateTodoButton'
+import { TodoCounter } from '../TodoCounter'
+import { TodoSearch } from '../TodoSearch'
+import { TodoList } from '../TodoList'
+import { TodoItem } from '../TodoItem'
+import { CreateTodoButton } from '../CreateTodoButton'
+import { useLocalStorage } from './useLocalStorage'
 import React from 'react'
-
-function useLocalStorage (itemName, initialValue) {
-  const localStorageItems = localStorage.getItem(itemName);
-  let parsedItem;
-  
-  if (!localStorageItems) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItems);
-  };
-
-  const [item, setItem] = React.useState(parsedItem);
-
-  const saveItem = (newItem) => {
-    localStorage.setItem(itemName, JSON.stringify(newItem));
-    setItem(newItem);
-  };
-
-  return [item, saveItem]
-}
 
 function App() {
   const [todos, saveTodos] = useLocalStorage('EJEMPLOS_TODOS', []);
