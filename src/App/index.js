8a74180b40f86f230a24bfbf52c034d813f4186b
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2023-12-01 23:33:54 -0300

    Clase 17 Curso de React.js

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,6 @@
-import { TodoCounter } from '../TodoCounter'
-import { TodoSearch } from '../TodoSearch'
-import { TodoList } from '../TodoList'
-import { TodoItem } from '../TodoItem'
-import { CreateTodoButton } from '../CreateTodoButton'
 import { useLocalStorage } from './useLocalStorage'
 import React from 'react'
+import { AppUI } from "./AppUI"
 
 function App() {
   const [todos, saveTodos] = useLocalStorage('EJEMPLOS_TODOS', []);
@@ -35,26 +31,15 @@ function App() {
   };
 
   return (
-    <>
-      <TodoCounter completed={completedTodos} total={totalTodos}/>
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-      
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem 
-          key={todo.text} 
-          text={todo.text} 
-          completed={todo.completed} 
-          onComplete={() => {completeTodo(todo.text)}}
-          onDelete = {() => {deleteTodo(todo.text)}}/>
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-    </>
+    <AppUI 
+    completedTodos={completedTodos}
+    totalTodos={totalTodos}
+    searchValue={searchValue}
+    setSearchValue={setSearchValue}
+    searchedTodos={searchedTodos}
+    completeTodo={completeTodo}
+    deleteTodo={deleteTodo}
+    />
   );
 };
 
